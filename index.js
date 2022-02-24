ca88829f4e78d3fc02da1d297bb657eb99329074
n4n5 <its.just.n4n5@gmail.com>
2022-02-24 22:06:08 +0100

    [feature] add methods

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ export default class ForemClient {
         this.requester = requester;
         this.API_ENDPOINT = endpoint || "https://dev.to/api"; // default
         this.API_KEY = "";
+        this.user = {};
     }
 
     setAPIkey = (newAPIkey) => {
         this.API_KEY = newAPIkey;
     }
-    /*
+    setUser = (newUser, override = true) => {
+        if (override) {
+            this.user = newUser;
+        } else {
+            this.user = { ...this.user, newUser };
+        }
+    }
+    getUser = () => {
+        return this.user;
+    }
+
+    /**
         @params options = {
             page,
             per_page,
@@ -27,14 +39,30 @@ export default class ForemClient {
         return await this.request('get', `/articles?${urlParams}`);
     }
 
-    /*
+    /**
+        getUserByUsername
+        @params username
+    */
+    GET_getUserByUsername = async (username = "") => {
+        return await this.request('get', `/users/by_username?url=${username}`);
+    }
+
+    /**
+        getUserById
+        @params id
+    */
+    GET_getUserById = async (id = 0) => {
+        return await this.request('get', `/users/${id}`);
+    }
+
+    /**
         Post an article
     */
     POST_articles = async (data = null) => {
         return await this.request('post', '/articles', data);
     }
 
-    /*
+    /**
         Update an article
     */
     PUT_articles = async (data = null) => {
@@ -42,13 +70,25 @@ export default class ForemClient {
         return await this.request('put', '/articles', data);
     }
 
+    /**
+        Update an article
+        @params options: {
+            page: 
+            per_page: 
+        }
+    */
+    GET_articlesMe = async (options) => {
+        const urlParams = new URLSearchParams(options).toString();
+        return await this.request('get', `/articles/me?${urlParams}`);
+    }
+
     async request(method = 'get', url = "", dataToSend = null) {
         const obj = {
-            method: method,
-            headers: {
-                "api-key": this.API_KEY
-            }
+            method: method
         };
+        if (this.API_KEY) {
+            obj.headers = { "api-key": this.API_KEY }
+        }
         if (dataToSend !== null) {
             obj.data = dataToSend;
         }
